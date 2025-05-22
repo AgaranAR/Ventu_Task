96f447dae084f73de253236b54919fac458b38aa
AgaranAR <agaran.ar2022@gmail.com>
2025-05-22 21:02:13 +0530

    Fix ratio

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
       <MapContainer
         center={[20.5937, 78.9629]}
         zoom={5}
-        style={{ height: "500px", width: "100%" }}
+        style={{ height: "500px", width: "1080px" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
